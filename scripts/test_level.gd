c00ea8597b725d67ad9032105d7328cf5ed96cb6
TheCrusty <zezoku@yahoo.com>
2023-07-08 11:26:51 -0400

    start button added that spawns a monster

--- scripts/test_level.gd
+++ scripts/test_level.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+var monster = preload("res://scenes/monster.tscn")
+
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
 	pass
+
+
+func _on_button_pressed():
+	var monster_instance = monster.instantiate()
+	add_child(monster_instance)
