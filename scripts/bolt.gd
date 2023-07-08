a772a441a876795de705b885745d1267d518d96a
Brandon Wood <brw422@g.harvard.edu>
2023-07-08 16:32:34 -0400

    added bolt sprite to tower bolt

---
+++ scripts/bolt.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var enabled = false
+@export var move_speed = 200
+@export var direction = 1
+
+func fire():
+	enabled = true
+	
+func _process(delta):
+	if enabled:
+		position.x = position.x + (move_speed * direction) * delta
+
+func _on_area_2d_area_entered(area):
+	print(area.name)
