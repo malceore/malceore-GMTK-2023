44cf89c7d1cd84b3c4866b5e9127699713fb0b1d
Brandon Wood <brw422@g.harvard.edu>
2023-07-08 10:24:40 -0400

    adding logic so you can purchase towers wip

---
+++ scripts/room_purchasing_menu.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@export var currentGold = 10
+@export var costsOfSlots = [
+	1,
+	3,
+	5,
+	0,
+	0,
+	0
+]
+var slots = {}
+
+func _ready():
+	# Gather up the slots, tying names to costs. Also set label costs
+	var slotCount = 0
+	for child in self.get_children():
+		if "Slot" in child.name:
+			if costsOfSlots[slotCount] != 0:
+				child.find_child("CostLabel").text = str(costsOfSlots[slotCount]) + " GP"
+			else:
+				child.find_child("CostLabel").text = " " 
+			slots[child.name] = costsOfSlots[slotCount]
+			slotCount += 1	
+
+	$GoldLabel.text = str(currentGold) + " GP"
+	
+
+func _process(delta):
+	$GoldLabel.text = str(currentGold) + " GP"
+
+func _on_slot_1_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		_on_slot_clicked(event, "Slot1")
+
+func _on_slot_clicked(event, name):
+	if currentGold >= slots[name]:
+		currentGold -= slots[name]
+		print("You have purchased this.")
