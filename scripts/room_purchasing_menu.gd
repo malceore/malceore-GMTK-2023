8b4343fe8ad70351965a003380d7de0c8f0191d5
Brandon Wood <brw422@g.harvard.edu>
2023-07-08 11:03:46 -0400

    made it so when you click on room button you get a room tracking your cursor. will need to swap this to a signal.

--- scripts/room_purchasing_menu.gd
+++ scripts/room_purchasing_menu.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+var placeHolderRoom = preload("res://scenes/room.tscn")
+
 @export var currentGold = 10
 @export var costsOfSlots = [1, 3, 5, 0, 0, 0]
 var slots = {}
+var holdingRoom = false
+var heldRoom = null
 
 func _ready():
 	# Gather up the slots, tying names to costs. Also set label costs
@@ -15,17 +19,19 @@ func _ready():
 				child.find_child("CostLabel").text = " " 
 			slots[child.name] = costsOfSlots[slotCount]
 			slotCount += 1	
-
 	$GoldLabel.text = str(currentGold) + " GP"
-	
 
 func _process(delta):
 	$GoldLabel.text = str(currentGold) + " GP"
+	if holdingRoom:
+		heldRoom.global_position = get_global_mouse_position()
 
 func _on_slot_clicked(event, name):
 	if currentGold >= slots[name] and (currentGold - slots[name]) > 0:
 		currentGold -= slots[name]
-		print("You have purchased this.")
+		heldRoom = placeHolderRoom.instantiate()
+		add_child(heldRoom)
+		holdingRoom = true
 
 func _on_slot_1_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
