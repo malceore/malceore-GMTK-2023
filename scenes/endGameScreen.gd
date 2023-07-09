f325ec29486321a8b440ecf719b527be5de4856f
TheCrusty <zezoku@yahoo.com>
2023-07-08 23:05:29 -0400

    loss screen implemented, no stats yet

--- scenes/endGameScreen.gd
+++ scenes/endGameScreen.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_try_again_pressed():
+	get_tree().change_scene_to_file("res://scenes/startMenu.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_quit_pressed():
+	get_tree().quit()
