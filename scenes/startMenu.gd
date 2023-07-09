d60165e5ff0efab2c8f9154a5bfcdf87f9ccba21
Brandon Wood <brw422@g.harvard.edu>
2023-07-09 09:52:25 -0400

    adding level 4

--- scenes/startMenu.gd
+++ scenes/startMenu.gd
@@ -8,3 +8,6 @@ func _on_level_2_pressed():
 
 func _on_level_3_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
+
+func _on_level_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_4.tscn")
